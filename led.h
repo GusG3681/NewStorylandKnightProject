411e0a14348232ae931cea2d2708228956428983
Gustavo Gomez <gusgomez3681@gmail.com>
2026-09-13 20:52:20 -0700

    Update led.h

--- led.h
+++ led.h
@@ -1,9 +1,10 @@
 #pragma once
 
 // Adjust GPIO pin numbers to match your breadboard wiring
-#define LedPin 0
-#define GREEN_PIN 23
-#define BLUE_PIN  24
+#define LEDPIN1 23
+#define LEDPIN1 24
+
+extern int status;
 
 void led_init();
 void led_on();
